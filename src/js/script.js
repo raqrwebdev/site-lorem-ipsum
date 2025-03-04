908bb6dd99936b3b09a73e5fce8e088257b10199
leticiaramosweb@gmail.com <ramossf@unicamp.br>
2025-03-04 00:09:17 -0300

    Commit inicial - Deploy da aplicação

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".responsive-menu").on("click", function () {
+    $(".responsive-menu-container").animate({
+        right: '0',
+    });
+  });
+
+  $(".responsive-menu-btn-close").on("click", function () {
+    $(".responsive-menu-container").animate({
+        right: '-100%',
+    });
+  });
+
+});
